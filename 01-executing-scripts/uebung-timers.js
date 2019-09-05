426c673d0995563445634934b471d51ede062637
eddex <eddex@outlook.com>
2019-09-05 13:58:11 +0200

    asynchronous timer exercise

--- 01-executing-scripts/uebung-timers.js
+++ 01-executing-scripts/uebung-timers.js
@@ -1,6 +1,23 @@
-const theOnlyFunction = delay => {
-
+const waitToSayHello = delay => {
+    setTimeout(() => console.log(`Hallo Web-Programming-Lab nach ${delay/1000} Sekunden`), delay);
 };
 
-// “Hallo Web-Programming-Lab nach 4 Sekunden”        
+// “Hallo Web-Programming-Lab nach 4 Sekunden”
+waitToSayHello(4000);
 // “Hallo Web-Programming-Lab nach 8 Sekunden”
+waitToSayHello(8000);
+
+// “Hallo Web-Programming-Lab nach 3 Sekunden”
+let intervalId = setInterval(() => waitToSayHello(3000), 3000);
+
+let count = 0;
+const sayHelloFiveTimes = () => {
+    console.log('HELLO!!! :^)');
+    count++;
+    if (count == 5) {
+        console.log('Done.');
+        clearInterval(intervalId);
+        process.exit();
+    }
+};
+setInterval(sayHelloFiveTimes, 1000);
